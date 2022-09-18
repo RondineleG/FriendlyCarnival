97dfbd38c162f641fec359214f30633db5973b87
Rondinele Guimaraes <rondineleg@outlook.com>
2022-09-18 09:28:59 -0300

    Partial Classes Implementation

---
+++ src/FriendlyCarnival.Client/Components/Home.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FriendlyCarnival.Client.Components
+{
+    public partial class Home
+    {
+        [Parameter]
+        public string Title { get; set; }
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public Dictionary<string, object> AdditionalAttributes { get; set; }
+
+        [CascadingParameter(Name = "HeadingColor")]
+        public string Color { get; set; }
+    }
+}
